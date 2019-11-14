2039d7ee77412cc667391d58ffbc6f4c65abf363
Donghyun Seo <donghyun.seo91@gmail.com>
2019-11-15 01:08:28 +0900

    feat: Add unit test for crawler
    * Add test for constant and Context in naver
    
    issue: #1

--- packages/core/__test__/domain/use-cases/stock-item/CrawlTotalPage.spec.ts
+++ packages/core/__test__/domain/use-cases/stock-item/CrawlTotalPage.spec.ts
@@ -1,7 +1,7 @@
 import { async } from 'rxjs/internal/scheduler/async';
 import CrawlTotalPage from '../../../../src/domain/use-cases/stock-item/CrawlTotalPage';
 import mockNaverStockItemRepository, { mockCrawlTotalPage } from '../../../../__mocks__/NaverStockItemRepository';
-import { KOSPI } from '../../../../lib/constant/market';
+import { KOSPI } from '../../../../src/constant/market';
 import { queue } from 'rxjs/internal/scheduler/queue';
 
 describe('CrawlTotalPage UseCase', () => {
