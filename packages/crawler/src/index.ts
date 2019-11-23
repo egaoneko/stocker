41af0ecb00aceffb6f4d0d35b1d18e4ec42a2084
Donghyun Seo <donghyun.seo91@gmail.com>
2019-11-23 19:30:33 +0900

    feat: Add last business day to krx crawler
    * Add last business day to krx crawler for crawling stock items
    
    issue: #7

--- packages/crawler/src/index.ts
+++ packages/crawler/src/index.ts
@@ -1,11 +1,5 @@
-import { KOSPI } from '@stocker/core/lib/constant/market';
 import { crawlStockItems } from './stock-item';
 import { crawlLastBusinessDay } from './stock';
-import { StockItemCrawlerType } from './stock-item/constant';
-
-crawlStockItems(StockItemCrawlerType.KRX, KOSPI).subscribe(
-  (res) => console.log(res)
-);
 
 export {
   crawlStockItems,
