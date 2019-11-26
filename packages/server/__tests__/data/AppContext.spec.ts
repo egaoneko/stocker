d9fb2defdc0c8e5b786242777e9994a548cb21ad
Donghyun Seo <donghyun.seo91@gmail.com>
2019-11-26 21:46:11 +0900

    feat: Implement AppContext
    issue: #23

---
+++ packages/server/__tests__/data/AppContext.spec.ts
@@ -0,0 +1,13 @@
+import AppContext from '../../src/data/AppContext';
+import SequelizeUserProvider from '../../src/data/db/providers/SequelizeUserProvider';
+import SquelizeUserRepository from '../../src/data/repositories/SquelizeUserRepository';
+import CreateUser from '@stocker/core/lib/domain/use-cases/account/CreateUser';
+
+describe('AppContext', () => {
+  test('constructor', () => {
+    const application: AppContext = new AppContext();
+    expect((application as any).providers.user).toBeInstanceOf(SequelizeUserProvider);
+    expect((application as any).repositories.user).toBeInstanceOf(SquelizeUserRepository);
+    expect((application as any).useCases.createUser).toBeInstanceOf(CreateUser);
+  });
+});
