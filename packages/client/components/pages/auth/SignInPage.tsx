836356a6276a3e61d34bb28ce8b907033ba8b3b4
Donghyun Seo <donghyun.seo91@gmail.com>
2020-01-15 23:22:40 +0900

    feat: Edit for menu and pages
    issue: #61

---
+++ packages/client/components/pages/auth/SignInPage.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import SignInTemplate from '../../../components/templates/account/SignInTemplate';
+import firebase from '../../../libs/firebase';
+import { CONTEXT } from '../../../constant';
+import { apply } from '@stocker/core/lib/utils/common';
+import CreateUser from '@stocker/core/lib/domain/use-cases/account/CreateUser';
+import FirebaseUserMapper from '../../../data/mappers/account/FirebaseUserMapper';
+import { async } from 'rxjs/internal/scheduler/async';
+import { queue } from 'rxjs/internal/scheduler/queue';
+import Router from 'next/router';
+
+interface PropsType {
+}
+
+const SignInPage: (props: PropsType) => JSX.Element = (): JSX.Element => {
+  const uiConfig: firebaseui.auth.Config = {
+    signInFlow: 'popup',
+    signInSuccessUrl: '/',
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: (credential: firebase.auth.UserCredential, redirectUrl: string) => {
+        if (credential && credential.user) {
+          apply<CreateUser>(
+            CONTEXT.useCases.createUser,
+            (it: CreateUser) => it.user = new FirebaseUserMapper().toEntity(credential.user as firebase.User)
+          )
+            .runOnce(async, queue)
+            .subscribe(() => {
+              Router.push('/');
+            }, (err: any) => {
+              console.error(err);
+              Router.push('/sign-in');
+            });
+        } else {
+          Router.push('/sign-in');
+        }
+        return false;
+      }
+    }
+  };
+
+  return (
+    <SignInTemplate uiConfig={uiConfig}/>
+  );
+};
+
+export default SignInPage;
